085ecdefb25e5c8b2f9376b22bb9a1e4ba8c5ed8
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-23 13:56:09 -0600

    Update product slug page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className='logo'>
-        <Link href='/'>Store Headphones</Link>
+        <Link href='/'>SS-Store Headphones</Link>
       </p>
       <button type="button" className='cart-icon' onClick="" >
         <AiOutlineShopping />
